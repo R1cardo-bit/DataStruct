cb0bdfdedf4336da66a58a0a15135e8f6abee06c
huzhipeng <huzhipeng@chagent.com>
2026-09-25 00:03:31 +0800

    折半查找2

--- LinkList/折半查找/main.c
+++ LinkList/折半查找/main.c
@@ -2,15 +2,29 @@
 
 #include<stdio.h>
 
+#define MAXSIZE 10
 typedef struct {
-    int MAXSIZE = 10;
-    
-}linklist;
+    int data[MAXSIZE];
+    int length;
+}sqllist;
 
-
-
-int main(){
-    while(true){
-        if()
+int fun(int target,sqllist q){
+    int left = 0;
+    int right = q.length - 1;
+    while(left <= right){
+        int mid = (left + (right - left) )/ 2;
+        if(mid > target){
+           right = mid - 1;
+        }else if (mid < target){
+            left = mid + 1;
+        }else{
+            return mid;
+        }
     }
+    return -1;
+}
+
+void main(){
+    sqllist q = {{1,2,4,8,9},5};
+    fun(2,q);
 }
